9eb444ca765555bf978a7b0e0e15a3e354483584
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-03-14 12:57:00 +0530

    setting up tailwind and building components layouts

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
 import Image from "next/image";
+import TicketCard from "./(components)/TicketCard";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className="lg:grid lg:grid-cols-2 xl:grid-cols-4">
+      <TicketCard/>
+      <TicketCard/>
+      <TicketCard/>
+      <TicketCard/>
+      <TicketCard/>
+      <TicketCard/>
+      </div>
+      
+    </div>
   );
 }
