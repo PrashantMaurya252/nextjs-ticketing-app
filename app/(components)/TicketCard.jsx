3e5c24f3073a6c185703a0e4ebf2bb31c992c46b
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-03-18 22:49:32 +0530

    adding dynamic data

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,20 +3,20 @@ import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
-const TicketCard = () => {
+const TicketCard = ({ticket}) => {
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
-        <PriorityDisplay />
+        <PriorityDisplay priority={ticket.priority}/>
         <div className="ml-auto">
           <DeleteBlock />
         </div>
       </div>
 
-      <h4>Ticket Title</h4>
+      <h4>{ticket.title}</h4>
       <hr className="h-px border-0 bg-page mb-2" />
       <p className="whitespace-pre-wrap">
-        This is ticket description ! Please do this ticket
+        {ticket.description}
       </p>
       <div className="flex-grow"></div>
       <div className="flex mt-2">
--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -3,11 +3,20 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   try {
-    const body=await req.json();
-    const ticketData=body.formData
-    await Ticket.create(ticketData)
-    return NextResponse.json({message:"Ticket Created"},{status:201})
+    const body = await req.json();
+    const ticketData = body.formData;
+    await Ticket.create(ticketData);
+    return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
   } catch (error) {
-    return NextResponse.json({message:"Error",error},{status:500})
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const tickets = await Ticket.find();
+    return NextResponse.json({tickets},{status:201});
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
 }
